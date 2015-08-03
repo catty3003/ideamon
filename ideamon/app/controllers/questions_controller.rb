f638c2be470edddecb7406ba34c1e3b6a5bfc418
catty3003 <catty_chan16@yahoo.com>
2015-08-03 05:03:58 +0200

    user identification wieder da; jetzt bleibt die app erst mal so

--- ideamon/app/controllers/questions_controller.rb
+++ ideamon/app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :set_question, only: [:show, :edit, :update, :destroy]
+  before_action :set_user_identification, only: [ :edit, :update, :destroy]
   before_action :set_admin, only: [:index]
   before_action :authenticate_user!
 
